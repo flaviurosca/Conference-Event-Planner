a772e20f04a7bc019874eae444fdc1e12d15f27b
flaviurosca <flaviuroscaivanciuc@gmail.com>
2024-09-17 04:07:09 -0400

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/Conference-Event-Planner",
+    plugins: [react()],
 })
