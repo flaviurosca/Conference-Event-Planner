74c6de56a6e9f7113d72827ccf8bc0dc85f07ba4
flaviurosca <flaviuroscaivanciuc@gmail.com>
2024-10-04 12:26:28 -0400

    finalizing project

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
-
+  const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
   return (
     <div className="pricing-app">
       <div className="display_box">
         <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
+          <p className="preheading">
+            <h3>Total cost for the event</h3>
+          </p>
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-           
+            ${total_amount}
           </h2>
-         
-            <div>
-             
-            </div>
+
+          <div className="render_items">
+            <ItemsDisplay />
+          </div>
         </div>
       </div>
     </div>
